42f9ae3f4d57b73e1b1db5539a424a2a6dcb4a10
Art Steinmetz <apsteinmetz@yahoo.com>
2024-01-28 16:17:07 -0500

    multi-page extraction

--- extract_table.R
+++ extract_table.R
@@ -2,48 +2,93 @@ library(tidyverse)
 library(pdftools)
 migration_raw <- pdf_data("data/immigration.pdf")
 
+
+data_start = 95
 footer_start = 690
-migration <- migration_raw[[14]] %>% 
-  filter(str_detect(text, " .",negate = TRUE)) %>% 
-  # remove footnote numbers
-  filter(height != 5) %>% 
-  # select(y,text) %>%
-  # remove headers
-  filter(y > 94) %>%
-  # remove footer
-  filter(y < footer_start) %>%
-  # tag row labels
-  mutate(is_rowlabel = ifelse(x < 100,TRUE,FALSE)) %>% 
-  # tag continent labels
-  mutate(is_continent = ifelse(x %in% c(39,40),TRUE,FALSE)) %>% 
-  # tag countries
-  mutate(is_region = ifelse(x %in% c(43,44),TRUE,FALSE)) %>% 
-  # tag sub-countries
-  mutate(is_subregion = ifelse(x %in% c(48,49),TRUE,FALSE)) %>% 
-  identity()
-  
-row_labels <- migration %>% 
-  filter(is_rowlabel)  %>% 
-  select(y,text) %>% 
-  group_by(y) %>% 
-  nest(region = text)
+col_label_row = 72
 
-
-data_cells <- migration %>%
-  filter(!is_rowlabel) %>% 
-  select(y,text) %>% 
-  group_by(y) %>% 
-  nest()
-  
-make_row <- function(row_data){
-  row_data %>% 
-    unlist() %>% 
-    paste(" ",collapse = '')
+make_row <- function(row_data) {
+  row_data %>%
+    unlist() %>%
+    paste(" ", collapse = '')
 }
 
 
-full_rows <- full_join(row_labels,data_cells,by = "y") %>% 
-  mutate(region = str_trim(make_row(region))) %>% 
-  mutate(data = make_row(data)) %>% 
-  # separate data into columns
-  separate_wider_delim(col=data,delim = "  ",names_sep = " decade")
+extract_page <- function(page) {
+  # DEBUG
+  # page <- migration_raw[[17]]
+  # detect bottom of table
+  footer_start <-
+    page %>% filter(str_detect(text, "footnote|Prior")) %>%
+    pull(y)
+  col_labels <- page %>%
+    filter(y == col_label_row) %>%
+    pull(text) %>%
+    paste0("s")
+  
+  migration <- page %>%
+    filter(str_detect(text, " .", negate = TRUE)) %>%
+    # remove footnote numbers
+    filter(height != 5) %>%
+    # select(y,text) %>%
+    # remove headers
+    filter(y >= data_start) %>%
+    # remove footer
+    filter(y < footer_start) %>%
+    # tag row labels
+    mutate(is_rowlabel = ifelse(x < 100, TRUE, FALSE)) %>%
+    # tag continent labels
+    mutate(is_continent = ifelse(x %in% c(39, 40), TRUE, FALSE)) %>%
+    # tag countries
+    mutate(is_region = ifelse(x %in% c(43, 44), TRUE, FALSE)) %>%
+    # tag sub-countries
+    mutate(is_subregion = ifelse(x %in% c(48, 49), TRUE, FALSE)) %>%
+    identity()
+  
+  row_labels <- migration %>%
+    filter(is_rowlabel)  %>%
+    select(y, text, contains("is")) %>%
+    group_by(y) %>%
+    nest(region = text) %>%
+    # this will create duplicates so use take just first
+    mutate(region = str_trim(make_row(region))) %>%
+    slice_head(n = 1)
+  
+  
+  data_cells <- migration %>%
+    filter(!is_rowlabel) %>%
+    select(y, text) %>%
+    rename(number = text) %>%
+    group_by(y) %>%
+    nest(data = number)
+  
+  # add decade labels
+  data_cells$data <- data_cells$data %>%
+    map(mutate, decade = col_labels, .before = "number")
+  
+  
+  full_rows <- full_join(row_labels, data_cells, by = "y") %>%
+    ungroup() %>%
+    select(-is_rowlabel) %>%
+    mutate(continent = "") %>%
+    arrange(y) %>%
+    # fix missing label rows
+    mutate(across(starts_with("is_"), \(x) ifelse(is.na(x), FALSE, x)))
+  
+  current_continent <- ""
+  for (i in 1:nrow(full_rows)) {
+    if (full_rows$is_continent[i]) {
+      current_continent <- full_rows$region[i]
+    }
+    full_rows$continent[i] <- current_continent
+  }
+  
+  return(full_rows)
+}
+
+migration <- migration_raw[14:17] %>% map(extract_page) %>% 
+  bind_rows() %>% 
+  unnest(cols = data) %>% 
+  group_by(region) %>% 
+  nest(data = c(decade,number)) %>% 
+  select(continent,region,data,everything())
